68621dcadcf02b59a98f83454ae1d071c11556c9
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-13 15:45:43 -0800

    began script for randomly generating question-brick groups and calculated necessary width for a specific randomly generated group

---
+++ Assets/QuestionBrickGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionBrickGroup : MonoBehaviour
+{
+    public int minBlocks, maxBlocks;
+    int numQuestionBlocks;
+    int numBlocks;
+    public GameObject brickPrefab, questionBlockPrefab;
+    float width;
+    void Start()
+    {
+        numBlocks = Random.Range(minBlocks, maxBlocks);
+        numQuestionBlocks = Random.Range(0,(int)numBlocks/2);
+        float questionBlockWidth = questionBlockPrefab.GetComponent<Collider>().bounds.size.z;
+        float brickWidth = brickPrefab.GetComponent<Collider>().bounds.size.z;
+        width = numQuestionBlocks * questionBlockWidth + (numBlocks - numQuestionBlocks) * brickWidth;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
