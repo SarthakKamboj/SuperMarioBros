605d8ca93c6dd13c4bd52fe7154a6b67bb9945f7
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-14 15:43:03 -0800

    added castle model and began coding mario moving to the castle upon level completion

---
+++ Assets/Scripts/MovePlayerToCastle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerToCastle : MonoBehaviour
+{
+    public Transform endTransform;
+    Vector3 endPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPos = endTransform.position;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
