47435717a9117c3206887794d413a038c2fdb120
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-15 15:29:16 -0800

    added restart functionality

--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -3,19 +3,25 @@
 public class PlayerDie : MonoBehaviour
 {
     public Vector3 upForce = new Vector3(0f,20f,0f);
+    public SceneHandler sceneHandler;
     GameObject audioManager;
 
     void Start() {
         audioManager = GameObject.Find("AudioManager");
     }
+    
+
     public void Die() {
-        audioManager.GetComponent<SoundHandler>().Play("Player Die");
-        StagnateMovement();
+        SoundHandler soundHandler = audioManager.GetComponent<SoundHandler>();
+        soundHandler.Play("Player Die");
+        BounceUp();
         GetComponent<DisablePlayer>().DisableAllComponents();
-        Destroy(gameObject, 5f);
+        sceneHandler.RestartLevel(soundHandler.GetClip("Player Die").length);
+        // Destroy(gameObject, 5f);
+
     }
 
-    void StagnateMovement() {
+    void BounceUp() {
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.AddForce(upForce,ForceMode.VelocityChange);
         GetComponent<Collider>().isTrigger = true;
--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -12,6 +12,13 @@ public class SceneHandler : MonoBehaviour
         StartCoroutine(_LoadNextScene(animTime));
     }
 
+    public void RestartLevel(float waitTime) {
+        Invoke("_RestartLevel", waitTime);
+    }
+
+    public void _RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     IEnumerator _LoadNextScene(float waitTime) {
         yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
