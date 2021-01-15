306372e4d76ddb7652787e7f1c2ff6e2b069f802
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-15 00:51:38 -0800

    started ui

---
+++ Assets/Scripts/BouncePlayerOff.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BouncePlayerOff : MonoBehaviour
+{
+    public float bounceMultipler = 2f;
+    void OnCollisionEnter(Collision col) {
+        if (col.collider.tag == "Player") {
+            Vector3 dir;
+            if (col.collider.transform.position.z <= transform.position.z) {
+                dir = new Vector3(0f,0f,-1f);
+            } else {
+                dir = new Vector3(0f,0f,1f);;
+            }
+            col.gameObject.GetComponent<Rigidbody>().AddForce(dir * bounceMultipler);
+        }
+    }
+}
