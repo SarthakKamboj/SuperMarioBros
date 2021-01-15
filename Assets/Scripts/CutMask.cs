9bee639d2b2714ce648ee34d1e8c7d1f95a08ed9
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-15 13:11:24 -0800

    created level begin/end masking animations and their respective scripts

---
+++ Assets/Scripts/CutMask.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class CutMask : Image
+{
+    public override Material materialForRendering  {
+        get {
+            Material mat = base.materialForRendering;
+            mat.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return mat;
+        }
+    }
+}
--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
 
 public class SceneHandler : MonoBehaviour
 {
+    public Animator loadAnimator;
 
     public void LoadNextScene() {
-       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        loadAnimator.SetTrigger("End");
+        float animTime = loadAnimator.GetCurrentAnimatorStateInfo(0).length;
+        StartCoroutine(_LoadNextScene(animTime));
+    }
+
+    IEnumerator _LoadNextScene(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
