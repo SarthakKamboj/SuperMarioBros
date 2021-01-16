09e135c50cac6ce1d129da947967b3d80d8fc766
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-15 18:01:43 -0800

    started menu scripts

---
+++ Assets/MenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    public void RegisterEvent(string name) {
+        switch (name) {
+            case "One Player":
+                Debug.Log("one player");
+                // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case "Quit":
+                Debug.Log("quit");
+                Application.Quit();
+                break;
+        }
+    }
+
+}
---
+++ Assets/SelectorHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectorHandler : MonoBehaviour
+{
+    public Image selectorImg;
+    public RectTransform[] selectionLocations;
+    int selection = 0;
+    void Start()
+    {
+        UpdateSelection();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            selection = (selection + 1) % selectionLocations.Length;
+            UpdateSelection();
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            selection = selection - 1;
+            if (selection < 0) {
+                selection = selectionLocations.Length - 1;
+            }
+            UpdateSelection();
+        }
+    }
+
+    void UpdateSelection() {
+
+        selectorImg.rectTransform.anchoredPosition3D = selectionLocations[selection].anchoredPosition3D;
+    }
+}
